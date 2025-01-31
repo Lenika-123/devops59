43991a1abbae4159d29159bf16372d07992f5962
Lenika-123 <steffrin.502259@sxcce.edu.in>
2025-01-31 10:52:44 +0530

    Add files via upload

---
+++ java/src/main/java/com/mycompany/java/Java.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.java;
+
+/**
+ *
+ * @author IT20
+ */
+import java.util.*;
+public class Java {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int a=sc.nextInt();
+        System.out.println("Enter the next value");
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("The Addition of two number is:"+c);
+    }
+}
